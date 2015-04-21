c0c2056b965cb2a3d1a78c680fc8fd5c37a1d9fd
mosesadg <moses.adg@gmail.com>
2015-04-21 09:53:40 -0400

    BugTracker - User Roles Added
    BugTracker - User Roles Added

---
+++ BugTrackerAM/Helpers/UserRolesHelper.cs
@@ -0,0 +1,75 @@
+﻿using BugTrackerAM.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BugTrackerAM.Helpers
+{
+    public class UserRolesHelper
+    {
+        private UserManager<ApplicationUser> manager =
+            new UserManager<ApplicationUser>(
+                new UserStore<ApplicationUser>(
+                    new ApplicationDbContext()));
+
+        private bool IsUserInrole(string userId, string roleName)
+        {
+            return manager.IsInRole(userId, roleName);
+
+        }
+
+        public IList<string> ListUSerRoles(string userId)
+        {
+        return manager.GetRoles(userId);
+
+        }
+
+        public bool AddUserToRole(string userId, string roleName)
+        {
+            var result = manager.AddToRole(userId, roleName);
+            return result.Succeeded;
+        }
+
+        public bool RemoveUserFromRole(string userId, string roleName)
+        {
+            var result = manager.RemoveFromRole(userId, roleName);
+            return result.Succeeded;
+        }
+
+        public IList<ApplicationUser> UsersInRole(string roleName)
+        {
+            var db = new ApplicationDbContext();
+            var resultList = new List<ApplicationUser>();
+            
+            foreach(var user in db.Users)
+            {
+                if (IsUserInrole(user.Id, roleName))
+                {
+                    resultList.Add(user);
+                }
+
+             }
+            return resultList;
+
+        }
+
+        public IList<ApplicationUser> UsersNotInRole(string roleName)
+        {
+            var resultList = new List<ApplicationUser>();
+
+            foreach (var user in manager.Users)
+            {
+                if (!IsUserInrole(user.Id, roleName))
+                {
+                    resultList.Add(user);
+
+                }
+            }
+            return resultList;
+        }
+
+     }
+}
---
+++ BugTrackerAM/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BugTrackerAM.Startup))]
+namespace BugTrackerAM
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
